436057d0d346388c62cd0d3c93c2f17827c9725b
LABPC1328 <carlosulcu44@gmail.com>
2024-05-08 07:15:33 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace vehiculos
+{
+    class Program
+    {
+        class Vehiculo
+        {
+            public string Marca { get; set; }
+            public string Modelo { get; set; }
+            public int Año { get; set; }
+
+            public Vehiculo(string marca, string modelo, int año)
+            {
+                Marca = marca;
+                Modelo = modelo;
+                Año = año;
+            }
+
+            public virtual void MostrarInformacion()
+            {
+                Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}, Año: {Año}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Vehiculo vehi = new Vehiculo("Toyota", "Corola", 2020);
+            vehi.MostrarInformacion();
+        }
+    }
+}
